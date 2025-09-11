f8124c349b670b954cb4b7a612bd31b628cd4a49
SergeevRoman <sero121292@gmail.com>
2025-09-11 19:48:19 +0300

    QA_Sergeev_Roman_variant_9

---
+++ source/groupBy.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Функция, группирующая объекты в массиве по заданному ключу
+ * @param {Array<Object>} array - массив объектов, которые нужно сгруппировать
+ * @param {String} key - ключ, по которому будет происходить группировка
+ * 
+ * @example
+ * const data = [
+ *   { id: 1, category: 'fruit', name: 'apple' },
+ *   { id: 2, category: 'fruit', name: 'banana' },
+ *   { id: 3, category: 'vegetable', name: 'carrot' }
+ * ];
+ * 
+ * // returns {
+ * //   fruit: [
+ * //     { id: 1, category: 'fruit', name: 'apple' },
+ * //     { id: 2, category: 'fruit', name: 'banana' }
+ * //   ],
+ * //   vegetable: [
+ * //     { id: 3, category: 'vegetable', name: 'carrot' }
+ * //   ]
+ * // }
+ * groupBy(data, 'category');
+ * 
+ * @returns {Object} - объект, где ключи — это уникальные значения указанного ключа из объектов массива, а значения — массивы объектов, принадлежащих к каждой группе
+ */
+
+function groupBy(array, key) {
+  return array.reduce((result, currentValue) => {
+    const groupKey = currentValue[key];
+
+    if (!result[groupKey]) {
+      result[groupKey] = [];
+    }
+    
+    result[groupKey].push(currentValue);
+    return result;
+  }, {});
+}
+
