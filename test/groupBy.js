0719100644336fbf43e9c58c9d96e5c6ab5dcd71
SergeevRoman <sero121292@gmail.com>
2025-09-17 00:41:41 +0300

    add new tests

--- test/groupBy.js
+++ test/groupBy.js
@@ -47,4 +47,61 @@ QUnit.module('Тестируем функцию groupBy', () => {
             ]
         }, 'Все объекты должны быть сгруппированы под одним значением');
     });
+
+    QUnit.test('Работает правильно с одним объектом', (assert) => {
+        const data =[
+            {id: 1, category: 'fruit', name: 'apple'}
+        ];
+        const result = groupBy(data, 'category');
+
+        assert.deepEqual(result, {
+            fruit: [
+                {id: 1, category: 'fruit', name: 'apple'}
+            ]
+        }, 'Один объект группируется под одним значением');
+    });
+
+    QUnit.test('Работает правильно при группировке по ключу имени', (assert) => {
+        const data = [
+            {id: 1, category: 'fruit', name: 'apple'},
+            {id: 2, category: 'fruit', name: 'orange'},
+            {id: 3, category: 'fruit', name: 'apple'},
+            {id: 4, category: 'vegetable', name: 'carrot'}
+        ];
+        const result = groupBy(data, 'name');
+
+        assert.deepEqual(result, {
+            apple: [
+                {id: 1, category: 'fruit', name: 'apple'},
+                {id: 3, category: 'fruit', name: 'apple'}
+            ],
+            orange: [
+                {id: 2, category: 'fruit', name: 'orange'} 
+            ],
+            carrot: [
+                {id: 4, category: 'vegetable', name: 'carrot'}
+            ]
+        }, 'Все объекты группируются по именам');
+    });
+
+    QUnit.test('Работает правильно по несуществующему ключу', (assert) => {
+        const data = [
+            { id: 1, category: 'fruit', name: 'apple' },
+            { id: 2, category: 'fruit', name: 'banana' },
+            { id: 3, category: 'vegetable', name: 'carrot' },
+            { id: 4, category: 'fruit', name: 'orange' },
+            { id: 5, category: 'vegetable', name: 'lettuce' }
+        ];
+        const result = groupBy(data, 'wrong_key');
+
+        assert.deepEqual(result, {
+            undefined: [
+                { id: 1, category: 'fruit', name: 'apple' },
+            { id: 2, category: 'fruit', name: 'banana' },
+            { id: 3, category: 'vegetable', name: 'carrot' },
+            { id: 4, category: 'fruit', name: 'orange' },
+            { id: 5, category: 'vegetable', name: 'lettuce' }
+            ]
+        }, 'Все объекты сгруппировались под одним неопределенным ключом');
+    });
 });
